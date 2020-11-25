2135a27895a9574ef42f36d9ea947621d2440a6d
Yuniel Acosta Pérez <yuniel.acosta@cav.desoft.cu>
2020-11-25 12:33:30 -0500

    Start the project.

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,35 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyHomePage());
+void main() => runApp(MyApp());
 
-class MyHomePage extends StatelessWidget {
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'DeliMeals',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Flutter App'),
+        title: Text('DeliMeals'),
       ),
       body: Center(
-        child: Text('Widget Playground!'),
+        child: Text('Navigation Time!'),
       ),
     );
   }
