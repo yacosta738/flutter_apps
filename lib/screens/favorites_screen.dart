d091112d5df0001a7281d5c119bd7075aaf745d8
Yuniel Acosta Pérez <yuniel.acosta@cav.desoft.cu>
2020-11-27 15:19:55 -0500

    Adding a Mark as Favorite Feature

--- lib/screens/favorites_screen.dart
+++ lib/screens/favorites_screen.dart
@@ -4,7 +4,7 @@ class FavoritesScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: Text('The Favorites'),
+      child: Text('You have no favorites yet - start adding some!'),
     );
   }
 }
