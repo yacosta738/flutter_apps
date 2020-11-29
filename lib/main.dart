dc215abacfcb78227b2b9dbcf48982d09e29bc1f
Yuniel Acosta Pérez <yuniel.acosta@cav.desoft.cu>
2020-11-28 22:31:22 -0500

    Exploring Alternative Provider Syntax

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ChangeNotifierProvider(
-      create: (context) => Products(),
+      create: (_) => Products(),
       child: MaterialApp(
           title: 'MyShop',
           theme: ThemeData(
