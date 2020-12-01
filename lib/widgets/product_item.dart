6bc8162bd3a2e6abd821d7a045fbb67d6388dd08
Yuniel Acosta Pérez <yuniel.acosta@cav.desoft.cu>
2020-12-01 15:25:26 -0500

    Snackbars & Undoing Add to Cart Actions

--- lib/widgets/product_item.dart
+++ lib/widgets/product_item.dart
@@ -44,7 +44,18 @@ class ProductItem extends StatelessWidget {
               color: Theme.of(context).accentColor,
               onPressed: () {
                 cart.addItem(product.id, product.price, product.title);
-                Scaffold.of(context).showSnackBar(snackbar);
+                Scaffold.of(context).hideCurrentSnackBar();
+                Scaffold.of(context).showSnackBar(SnackBar(
+                  content: Text(
+                    'Added item to cart!',
+                    textAlign: TextAlign.center,
+                  ),
+                  duration: Duration(seconds: 2),
+                  action: SnackBarAction(
+                    label: 'UNDO',
+                    onPressed: () => cart.removeSingleItem(product.id),
+                  ),
+                ));
               }),
         ),
       ),
